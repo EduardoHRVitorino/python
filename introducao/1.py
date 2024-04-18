e3b77648b80127bc79467167343f773e5d15b1a8
EduardoHRVitorino <prometheus29_09@proton.me>
2024-04-18 15:04:17 -0300

    17/04 20:00

--- introducao/1.py
+++ introducao/1.py
@@ -1,3 +1,7 @@
-nome = input("informe seu nome: ")
-datanasc = input("digite sua data de nascimento: ")
-idade = input("digite sua idade:  ")
+nome = input("qual é o seu nome? \n")  #definição de variável
+ano = int(input("qual o ano atual?\n")) #definição de variável
+idade = int(input("qual é o seu ano de nascimento?\n")) #definição de variável
+subtracao = ano - idade #definição de variável
+
+print("ola %s voce possui em torno de %d anos" % (nome, subtracao)) #imprimir mensagem
+
---
+++ introducao/2.py
@@ -0,0 +1,4 @@
+valor = eval(input("digite um valor")) #eval é feita para multiplas variaveis e para identificar o tipo da variavel usando o type
+print(type(valor))
+
+print("teste de quebra de linha ")
