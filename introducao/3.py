8aea3c81406054e22a1405971339054968837377
EduardoHRVitorino <prometheus29_09@proton.me>
2024-05-12 18:56:53 -0300

    12/05 18:56

---
+++ introducao/3.py
@@ -0,0 +1,9 @@
+#condições e decisões no python são simples caminhando junto com as variáveis.
+#normalmente são utilizados alguns simbolos para sinalizar ao computador as ações de tomada de decisão
+#como > = maior que; < = menor que; >= = maior ou igual a; <= = menor ou igual a; == = igual a; != = diferente de;
+eduardo = input("Eduardo é uma pessoa ?")
+
+
+
+if(eduardo == "sim"): #o comando "if" é utilizado para impor condições para o computador determinar se 
+    print("de acordo com você, eduardo é uma pessoa")
